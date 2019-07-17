89dc2f3a6863706bcca787bc8a8a86fe5f66856d
smhaaris <32506500+smhaaris@users.noreply.github.com>
2019-07-17 13:54:01 +0530

    Create pipeline.groovy

---
+++ pipeline.groovy
@@ -0,0 +1,60 @@
+def function(props) {
+	stage('CheckoutProject') {
+		app_url =props.JAVA_APP_REPO_GIT_URL		
+		git "${app_url}"
+		pom = readMavenPom file: props.POM_FILE
+		artifactId=pom.artifactId
+		echo "${artifactId}"
+		version=pom.version
+		
+	}
+	stage('SonarAnalysis')
+	{
+	commonUtility.sonar();
+	}
+	stage("SonarQube Quality Gate")
+	{
+	commonUtility.qualityGate();
+	}
+	stage('BuildProject') 
+	{
+	sh props.MAVEN_BUILD		
+   	}
+	stage('UploadArtifactory') {
+	commonUtility.uploadArtifact();
+	}
+	stage('downloadingArtifact')
+	{
+	commonUtility.downloadArtifact();	
+	}
+	stage('Build & Push Docker image')
+	{
+	  sh props.DOCKER_BUILD
+	  sh props.DOCKER_TAG		
+	  sh props.DOCKER_PUSH
+	}	
+	stage('Dev deploy') {
+    	 echo 'Docker Deploy'
+         sh props.DOCKER_CMD
+  	 sh props.DOCKER_RUN
+	}
+	stage('Test deploy') {
+	input "Deploy to Test? "
+	echo 'Docker-compose Deploy'
+         sh props.ANSIBLE_CMD
+  	 sh props.ANSIBLE_RUN
+	}	
+	stage('Prod Deploy') {
+        input "Deploy to Production? "
+    	echo 'Deploy to kubernetes'
+     	sh props.KUBERNETES_APPLY
+     	sh props.KUBERNETES_GET_ALL
+	}
+	
+	
+	stage('Email Notification')
+	{
+		commonUtility.sendEmail();
+	}
+	}
+return this
