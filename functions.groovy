d05c540d8a0220cb95efcc79bc75e9b25301e58a
smhaaris <32506500+smhaaris@users.noreply.github.com>
2019-07-21 14:30:47 +0530

    Update functions.groovy

--- functions.groovy
+++ functions.groovy
@@ -39,12 +39,10 @@ def sonar(){
 	   }
 }
 def qualityGate() {
-	timeout(time: 10, unit: 'MINUTES') {
 		def qg = waitForQualityGate() 
            	if (qg.status != 'OK') {
              		error "Pipeline aborted due to quality gate failure: ${qg.status}"
            	}
-	}
 }
 
 def sendEmail() {
